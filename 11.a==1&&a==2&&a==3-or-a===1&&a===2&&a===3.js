713a36d76e7abd6699856a63a1dbdfb1714d8efe
xoyimi <xoyimi@foxmail.com>
2022-04-08 18:29:05 +0800

    feat: 增加 Symbol.toPrimitive 方式

--- 11.a==1&&a==2&&a==3-or-a===1&&a===2&&a===3.js
+++ 11.a==1&&a==2&&a==3-or-a===1&&a===2&&a===3.js
@@ -1,17 +1,29 @@
-// Object.defineProperty
-globalThis.count = 1
+//利用 Object.defineProperty
+const count = Symbol('count')
+globalThis[count] = 1
 Object.defineProperty(globalThis, 'a', {
   get() {
-    return globalThis.count++
+    return globalThis[count]++
   },
 })
 console.log(a === 1 && a === 2 && a === 3)
 
-// toString()
+// 利用 JS 的对象 -> 原始值转换 toString() / valueOf()
 const a = {
   count: 1,
   toString() {
     return this.count++
   },
 }
+console.log(a == 1 && a == 2 && a == 3)
+
+// 利用 JS 的对象 -> 原始值转换 Symbol.toPrimitive
+
+const a = {
+  count: 1,
+  [Symbol.toPrimitive]() {
+    return this.count++
+  },
+}
+
 console.log(a == 1 && a == 2 && a == 3)
